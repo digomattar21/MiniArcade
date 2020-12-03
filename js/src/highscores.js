79c7f1fc16eca5434102759a7d119102ce8c2f2c
digomattar21 <digomattar@gmail.com>
2020-12-03 18:22:04 -0300

    gh pages test3

--- js/src/highscores.js
+++ js/src/highscores.js
@@ -34,7 +34,7 @@ myform.addEventListener('submit', (event) =>{
     var formData = new FormData(myform);
     formData.append("score", score)
 
-    fetch("/js/src/scores.php",{
+    fetch("./scores.php",{
         method:"post",
         body: formData
     })
@@ -53,7 +53,7 @@ myform.addEventListener('submit', (event) =>{
 let scores;
 
 function get_scores(callback){
-    let file = '/js/src/scores.json';
+    let file = './scores.json';
     fetch(file,{cache:'no-cache'})
     .then(function(response){
         if (response.status !==200){
